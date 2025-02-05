2b8863f048adcf4dfb4bfb2e01cc2facdbfbeb4d
amritha-sabu <amritha.g.sabu@gmail.com>
2025-02-05 20:40:55 +0530

    Building Hero Section

--- booking-app/src/components/HomePage/Hero.jsx
+++ booking-app/src/components/HomePage/Hero.jsx
@@ -7,7 +7,33 @@ const Hero = () => {
         <div className="hero">
             <div className="hero_container">
                 <Navbar />
-                <div className="hero_content"></div>
+                <div className="hero_content">
+                    <p className="check_in_out">
+                    <label  htmlFor="check_in_date">Check In </label>
+                    <input className="check_in_date" type="date" id="check_in_date" name="check_in_date" required />
+                    </p>
+                    <p className="check_in_out">
+                    <label  htmlFor="check_out_date">Check Out </label>
+                    <input className="check_out_date" type="date" id="check_out_date" name="check_out_date" required />
+                    </p>
+                    <label htmlFor="no_of_adults">Adults</label>
+                    <select className="booking_dropdown" id="no-of-adults" name="no_of_adults" >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    </select>
+                    <label htmlFor="no_of_children">Children</label>
+                    <select className="booking_dropdown" id="no-of-children" name="no_of_children" >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    </select>
+
+                </div>
             </div>
         </div>
     );
