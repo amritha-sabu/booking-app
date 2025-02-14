1e2038afb8a8df1a5c890857fa2248d0af61de80
amritha-sabu <amritha.g.sabu@gmail.com>
2025-02-14 12:11:17 +0530

    Building Hero Section

---
+++ booking-app/src/components/HomePage/Calendar/Calendar.jsx
@@ -0,0 +1,9 @@
+const Calendar = () => {
+    return(
+        <div className="calendar">
+            <h1>Hi</h1>
+        </div>
+    );
+};
+
+export default Calendar;
--- booking-app/src/components/HomePage/Hero.jsx
+++ booking-app/src/components/HomePage/Hero.jsx
@@ -1,21 +1,34 @@
-// import hero from '../../assets/Hero.jpg';
 import Navbar from './Navbar';
+import Calendar from './Calendar/Calendar';
 import './Hero.css'
+import { useState } from 'react';
 
 const Hero = () => {
+    const [showCalendar, setShowCalendar] = useState(false);
+
+    const handleClick = () => {
+        setShowCalendar(!showCalendar);
+    }
+
     return (
         <div className="hero">
             <div className="hero_container">
                 <Navbar />
                 <div className="hero_content">
-                    <p className="check_in_out">
+                    <div className='check-in' onClick={handleClick}>
+                        <button>Check In</button>
+                    </div>
+                    <div className='check-out' onClick={handleClick}>
+                        <button>Check Out</button>
+                    </div>
+                    {/* <p className="check_in_out">
                     <label  htmlFor="check_in_date">Check In </label>
                     <input className="check_in_date" type="date" id="check_in_date" name="check_in_date" required />
                     </p>
                     <p className="check_in_out">
                     <label  htmlFor="check_out_date">Check Out </label>
                     <input className="check_out_date" type="date" id="check_out_date" name="check_out_date" required />
-                    </p>
+                    </p> */}
                     <label htmlFor="no_of_adults">Adults</label>
                     <select className="booking_dropdown" id="no-of-adults" name="no_of_adults" >
                     <option>1</option>
@@ -32,8 +45,8 @@ const Hero = () => {
                     <option>4</option>
                     <option>5</option>
                     </select>
-
                 </div>
+                    {showCalendar && <Calendar />}
             </div>
         </div>
     );
